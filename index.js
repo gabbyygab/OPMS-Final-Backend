425949c2a692a14c2a002a964e37ade7a9a080da
gabbyygab <delicanagabriel1212@gmail.com>
2025-10-20 14:28:46 +0800

    commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://bookingnest.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
   })
