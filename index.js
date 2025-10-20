07a77653225492d5ebed8aa168cb011db9a7a3e2
gabbyygab <delicanagabriel1212@gmail.com>
2025-10-20 14:30:59 +0800

    commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/api/paypal", paypalRoutes);
 app.get("/", (req, res) => {
   res.send("✅ PayPal backend running");
 });
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(` Server running on port ${port}`);
 });
